14d17e539ae97c94d8063b0a8ddb928c3523ca53
Fachrul Pralienka BM <fachrul.pbm@gmail.com>
2018-05-13 09:23:44 +0700

    login volley

---
+++ app/src/main/java/com/example/fachr/loginwithvolley/AppVar.java
@@ -0,0 +1,11 @@
+package com.example.fachr.loginwithvolley;
+
+public class AppVar {
+
+    public static final String LOGIN_URL = "http://192.168.100.4/loginvolley/login.php";
+
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+
+    public static final String LOGIN_SUCCESS = "succes";
+}
